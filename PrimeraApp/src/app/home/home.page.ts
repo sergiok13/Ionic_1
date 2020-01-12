098a00a8ce01c928ba750ee176842da8340085fd
LAPTOP-M62VF5F0\Sergio-P <sergiotorrome09@gmail.com>
2020-01-12 16:55:26 +0100

    AE7 (Sin gitignore)

---
+++ PrimeraApp/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  oculto : boolean = false;
+  brag: boolean = false;
+  motor : boolean = false;
+  inmobiliaria : boolean = false;
+  constructor() {}
+
+}
+
+
